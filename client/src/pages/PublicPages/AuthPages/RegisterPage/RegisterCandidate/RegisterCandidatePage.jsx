b57a9e047f9e6e73a5527bfbdb37d9c68b4af127
Enrique Alguacil <enriquealguacilm@gmail.com>
2026-05-19 18:18:04 +0200

    register candidate

--- client/src/pages/PublicPages/AuthPages/RegisterPage/RegisterCandidate/RegisterCandidatePage.jsx
+++ client/src/pages/PublicPages/AuthPages/RegisterPage/RegisterCandidate/RegisterCandidatePage.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import {Button, Form} from 'react-bootstrap';
 import { validateForm } from '../../../../../helpers/ValidateForms';
 import { registerCandidateSchema } from '../../../../../schemas/RegisterCandidateSchema';
-
-
+import { fetchAxios } from '../../../../../helpers/axiosHelper';
+import { Link } from 'react-router';
 
 const initialValue = {
   email: "",
@@ -13,13 +13,9 @@ const initialValue = {
   name: "",
   lastname: "",
   phone_number: ""
-
 }
 
-
-
 const RegisterCandidatePage = () => {
-
 
   const [registerCandidate, setRegisterCandidate] = useState(initialValue);
   const [errorsVal, setErrorsVal] = useState();
@@ -27,35 +23,37 @@ const RegisterCandidatePage = () => {
 
   const handleChange = (e) => {
     const {name, value} = e.target;
-
     setRegisterCandidate({...registerCandidate, [name]:value});
-
   }
 
   const onSubmit = async() => {
     setErrorsVal({});
     setOtroError("");
+
     try {
       validateForm(registerCandidateSchema,registerCandidate);
-      let url = "auth/register";
+      let url = "/auth/registerCandidate";
+      let res = await fetchAxios(url,"POST",registerCandidate);
+      console.log(res);
+      
 
+
+      
     } catch (error) {
       if (error.errType === "validator"){
         console.log("errores de validación");
         setErrorsVal(error);
        
-        
       }else if (error.response.data.errno === 1062){
           setOtroError("Email duplicado");
       }
       else {
-        setOtroError("Ha habido un error")
+        setOtroError("Ha habido un error");
       }
     }
   }
 
-  
-  
+
   return (
      <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -65,8 +63,8 @@ const RegisterCandidatePage = () => {
             onChange={handleChange}
             name='email'
             value={registerCandidate.email} 
-            
             />
+            {errorsVal?.email && <p className="errMsg">{errorsVal.email}</p>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicRepEmail">
         <Form.Label>Confirmar correo*</Form.Label>
@@ -77,8 +75,8 @@ const RegisterCandidatePage = () => {
             name='repEmail'
             value={registerCandidate.repEmail} 
             />
+            {errorsVal?.repEmail && <p className="errMsg">{errorsVal.repEmail}</p>}
       </Form.Group>
-
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Crea tu contraseña*</Form.Label>
         <Form.Control 
@@ -88,6 +86,7 @@ const RegisterCandidatePage = () => {
             name='password'
             value={registerCandidate.password} 
             />
+            {errorsVal?.password && <p className="errMsg">{errorsVal.password}</p>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicRepPassword">
         <Form.Label>Confirmar contraseña*</Form.Label>
@@ -98,6 +97,7 @@ const RegisterCandidatePage = () => {
             name='repPassword'
             value={registerCandidate.repPassword} 
             />
+            {errorsVal?.repPassword && <p className="errMsg">{errorsVal.repPassword}</p>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Nombre*</Form.Label>
@@ -106,8 +106,8 @@ const RegisterCandidatePage = () => {
             onChange={handleChange}
             name='name'
             value={registerCandidate.name} 
-            
             />
+            {errorsVal?.name && <p className="errMsg">{errorsVal.name}</p>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicLastname">
         <Form.Label>Apellido(s)*</Form.Label>
@@ -116,8 +116,8 @@ const RegisterCandidatePage = () => {
             onChange={handleChange}
             name='lastname'
             value={registerCandidate.lastname} 
-            
             />
+            {errorsVal?.lastname && <p className="errMsg">{errorsVal.lastname}</p>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
         <Form.Label>Teléfono*</Form.Label>
@@ -126,18 +126,20 @@ const RegisterCandidatePage = () => {
             onChange={handleChange}
             name='phone_number'
             value={registerCandidate.phone_number}  
-            
             />
+            {errorsVal?.phone_number && <p className="errMsg">{errorsVal.phone_number}</p>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Acepto la política de Privacidad y los Términos y Condiciones" />
       </Form.Group>
+      <p className="errMsg">{otroError}</p>
       <Button variant="primary" onClick={onSubmit}>
         Crear Mi Cuenta
       </Button>
       <Button variant="primary">
         Cancelar
       </Button>
+      <p>¿Ya estás registrado? <Link to='/login'>Login aquí</Link></p>
     </Form>
   )
 }
