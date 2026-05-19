5c13dc5f88c0335b5aab44c636d577880e1a3c09
Enrique Alguacil <enriquealguacilm@gmail.com>
2026-05-19 14:14:39 +0200

    carpeta pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import { AuthContextProvider } from './context/AuthContextProvider'
+import { AppRoutes } from './routes/AppRoutes'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <AuthContextProvider>
+      <AppRoutes/>
+    </AuthContextProvider>
   )
 }
 
