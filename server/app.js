27032bfe534c5f29794882fd5af711370010def2
Alejandro Salado <alex.salsan97@gmail.com>
2026-05-20 13:58:25 +0200

    envío de email correcto

--- server/app.js
+++ server/app.js
@@ -6,24 +6,11 @@ import logger from 'morgan';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 
-
- import createError from 'http-errors';
- import express from 'express';
- import path from 'path';
- import cookieParser from 'cookie-parser';
- import logger from 'morgan';
- import cors from 'cors';
- import { fileURLToPath } from 'url';
-
- import dotenv from 'dotenv';
- dotenv.config();
-
- import authRouter from './modules/auth/auth.routes.js'
-
- const __filename = fileURLToPath(import.meta.url);
- const __dirname = path.dirname(__filename);
-
-
+import dotenv from 'dotenv';
+dotenv.config();
+import authRouter from './modules/auth/auth.routes.js'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
